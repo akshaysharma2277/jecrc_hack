bd19bcde6459208c1b3eb35528b4a5c1a3ace307
akshaysharma2277 <akshaysharma2277@gmail.com>
2017-12-29 20:24:06 +0530

    Sample complaint portal

---
+++ javascript/main.js
@@ -0,0 +1,44 @@
+var config = {
+    apiKey: "AIzaSyCss2YpS6Orxyb76nJhWUam0NUitu2dN64",
+    authDomain: "jecrc-hackathon.firebaseapp.com",
+    databaseURL: "https://jecrc-hackathon.firebaseio.com",
+    projectId: "jecrc-hackathon",
+    storageBucket: "",
+    messagingSenderId: "1043386055663"
+};
+firebase.initializeApp(config);
+
+var messagesRef = firebase.database().ref('messages');
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+function submitForm(e){
+  e.preventDefault();
+
+  var name = getInputVal('name');
+  var company = getInputVal('company');
+  var email = getInputVal('email');
+  var phone = getInputVal('phone');
+  var message = getInputVal('message');
+  saveMessage(name, company, email, phone, message);
+  document.querySelector('.alert').style.display = 'block';
+
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  document.getElementById('contactForm').reset();
+}
+
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+function saveMessage(name, company, email, phone, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    company:company,
+    email:email,
+    phone:phone,
+    message:message
+  });
+}
